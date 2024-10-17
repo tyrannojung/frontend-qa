0fa4e89e556e645af122ce01e762d0b8561cb627
tyrannojung <tyranno2020@gmail.com>
2024-10-17 11:40:07 +0900

    build: set up initial Turborepo monorepo package configuration

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,20 @@
-/** @type {import("eslint").Linter.Config} */
+const path = require('path');
+
+/** @type {import("eslint").Liter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@repo/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    project: path.resolve(__dirname, 'tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: path.resolve(__dirname, 'tsconfig.json'),
+      },
+    },
   },
 };
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,40 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "turbo",
-  ],
+  extends: ['plugin:@next/next/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
   globals: {
     React: true,
     JSX: true,
   },
   env: {
-    node: true,
     browser: true,
+    node: true,
+    es6: true,
   },
-  plugins: ["only-warn"],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project,
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['.*.js', 'node_modules', '*.config.js', '*.config.mjs'],
+  overrides: [
+    {
+      files: ['*.js?(x)', '*.ts?(x)'],
+    },
   ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+  rules: {
+    // 여기에 필요한 규칙들을 추가하세요
+  },
 };
