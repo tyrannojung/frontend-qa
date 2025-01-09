515b5494e5e98c461c3717f25cf480ce73dfae01
Dawoon Jung <tyranno2020@gmail.com>
2025-01-09 15:18:22 +0900

    fix: update paths to src directory and related configurations

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -5,13 +5,16 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
-  rootDir: '.',
+  rootDir: './src',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/app/(.*)$': '<rootDir>/app/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/types/(.*)$': '<rootDir>/types/$1',
   },
-  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/cypress/', '/playwright/'],
 };
